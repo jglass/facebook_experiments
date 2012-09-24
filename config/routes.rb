9c8c8cb7ff95071158eda4aebc54e6c28815c225
jglass <glassj67@gmail.com>
2012-09-24 15:43:39 -0700

    Adds picture of piglet in sweater

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FacebookExperiments::Application.routes.draw do
+  resources :stories
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ FacebookExperiments::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+    root :to => 'stories#index'
 
   # See how all your routes lay out with "rake routes"
 
